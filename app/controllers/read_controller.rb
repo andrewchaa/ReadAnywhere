447e2a3328b390eba341612101e859d3dcd29ea7
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2012-02-28 23:03:05 +0000

    clean-up version

---
+++ app/controllers/read_controller.rb
@@ -0,0 +1,10 @@
+class ReadController < ApplicationController
+
+  # GET /read/1
+  def show
+    respond_to do |format|
+      format.html
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReadAnywhere::Application.config.secret_token = 'b353e1bde6542c67a6755a193d497aea122736b0f0eabefb8b7beb12de1ec7d230929cfc1c373c4cdbb8339b2c8db3d30e976bc1cacdd124989d738fa0e5d4d1'
